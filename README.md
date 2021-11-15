29bb567ce7a174af354bcc34a0fb3000cc6a317f
Yelban <yelban@gmail.com>
2021-11-15 14:14:03 +0800

    basic html/js/css boilerplate

---
+++ README.md
@@ -0,0 +1,1 @@
+# Vanilla JavaScript Dev Boilerplate
---
+++ src/public/js/index.js
@@ -0,0 +1,12 @@
+console.log('index.js');
+
+// <script src="./js/index.js" async defer> 
+// async after 'DOMContentLoaded'
+// defer before 'DOMContentLoaded'
+window.addEventListener('DOMContentLoaded', () => {
+    console.log('ready');
+});
+
+window.addEventListener("load", function(event) {
+    console.log("load");
+});
